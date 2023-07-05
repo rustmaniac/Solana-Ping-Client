bbd76f58f6ef7dfec61e4935bb4ac54c4b441a43
rustmaniac <rustmaniac3315@protonmail.com>
2023-07-05 09:27:22 +0900

    sending sol to specific address added

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,34 @@ Dotenv.config()
 
 const PROGRAM_ADDRESS = 'ChT1B39WKLS8qUrkLvFDXMhEJ4F1XZzwUNHUt4AU9aVa'
 const PROGRAM_DATA_ADDRESS = 'Ah9K7dQ8EHaZqcAsgBW8w37yN2eAy3koFmUn4x3CJtod'
+const RECEIVER_ADDRESS = 'HohukPnm58HgCQsn4SeZaq3shxAb17uDsf2fUHAKeiFo'
 
 async function main() {
     // const newKeypair = web3.Keypair.generate()
     // console.log(newKeypair.secretKey.toString())
     const payer = initializeKeypair()
     const connection = new web3.Connection(web3.clusterApiUrl('devnet'))
-    await connection.requestAirdrop(payer.publicKey, web3.LAMPORTS_PER_SOL * 1)
+    // await connection.requestAirdrop(payer.publicKey, web3.LAMPORTS_PER_SOL * 1)
     await pingProgram(connection, payer)
+    await sendSol(connection, 0.8 * web3.LAMPORTS_PER_SOL, new web3.PublicKey(RECEIVER_ADDRESS), payer)
+}
+
+async function sendSol(connection: web3.Connection, amount: number, to: web3.PublicKey, sender: web3.Keypair) {
+    const transaction = new web3.Transaction()
+    const sendSolTransaction = web3.SystemProgram.transfer({
+        fromPubkey: sender.publicKey,
+        toPubkey: to,
+        lamports: amount
+    })
+    transaction.add(sendSolTransaction)
+
+    const signature = await web3.sendAndConfirmTransaction(
+        connection,
+        transaction,
+        [sender]
+    )
+
+    console.log(signature)
 }
 
 async function pingProgram(connection: web3.Connection, payer: web3.Keypair) {
